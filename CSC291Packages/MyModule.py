2075d1139e5d45e6c7eac136e789a9183a929a70
Wesley Freeman <22487192+wesfree163@users.noreply.github.com>
2023-02-16 23:56:12 -0500

    Create MyModule.py

---
+++ CSC291Packages/MyModule.py
@@ -0,0 +1,6 @@
+############################################################
+# BUT WHAT SORT OF CODE GOES INTO A MODULE                 #
+# visit .ocs.python.org/3/reference/import.html#regular... #
+#  they encourage including an __init__.py file            #
+#  in the directory with the custom module                 #
+############################################################
